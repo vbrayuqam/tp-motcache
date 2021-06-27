e41127a11660378427c1f9624e274a1b511aac29
hj891126 <hj891126@ens.uqam.ca>
2021-06-27 12:09:28 -0400

    Fonction pour verifier les arguments

--- motcache.c
+++ motcache.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Definir les fonctions
+void verificationNbArguments(int nbArguments);
+
 //Implementer les fonctions
+void verificationNbArguments(int nbArguments) {
+   if(nbArguments != 2) {
+      perror("\nNombre d'arguments érroné, ce programme n'accepte qu'un argument.\n");
+      exit(-1);
+   }
+}
 
 void main(int argc, char *argv[]) {
-   //Definir les variables
+   verificationNbArguments(argc);
    //Lecture de la grille
       //Creation grille de mots
       //Creation grille de valeurs
