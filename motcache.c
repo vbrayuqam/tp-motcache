049908dfa136a63c38e0674e779dc6dbb9ed5c77
hj891126 <hj891126@ens.uqam.ca>
2021-06-27 11:27:46 -0400

    Definition de la structure du programme

---
+++ motcache.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Definir les fonctions
+//Implementer les fonctions
+
+void main(int argc, char *argv[]) {
+   //Definir les variables
+   //Lecture de la grille
+      //Creation grille de mots
+      //Creation grille de valeurs
+      //Creation table de mots a trouver
+   //Parcourir la grille et trouvers les mots
+   //Definir le mot de sortie
+   //Afficher la sortie
+}
