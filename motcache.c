55bd187bee0fde5ccbaef769791608b550d64a96
hj891126 <hj891126@ens.uqam.ca>
2021-06-27 14:11:38 -0400

    Ajout de la fonction d'initialisation de la grille des valeurs.

--- motcache.c
+++ motcache.c
@@ -5,6 +5,7 @@
 void verificationNbArguments(int nbArguments);
 void verificationFichierEntree(char cheminFichier[]);
 void lectureFichier(char cheminFichier[], char grille[12][12]);
+void initialisationGrilleValeurs(int grille[12][12]);
 
 //Implementer les fonctions
 void verificationNbArguments(int nbArguments) {
@@ -38,6 +39,14 @@ void lectureGrille(char cheminFichier[], char grille[12][12]) {
    fclose(fptr);
 }
 
+void initialisationGrilleValeurs(int grille[12][12]) {
+   for(int i = 0; i < 12; i++) {
+      for(int j = 0; j < 12; j++) {
+         grille[i][j] = 0;
+      }
+   }
+}
+
 //Fonction main
 void main(int argc, char *argv[]) {
    char grilleLettres[12][12];
@@ -46,11 +55,19 @@ void main(int argc, char *argv[]) {
    verificationNbArguments(argc);
    verificationFichierEntree(argv[1]);
    lectureGrille(argv[1], grilleLettres);
-    
+   initialisationGrilleValeurs(grilleValeurs);
+
    //Test de grilleLettres
    for(int i = 0; i < 12; i++) {
       for(int j = 0; j < 12; j++) {
          printf("%c", grilleLettres[i][j]);
+      }
+      printf("\n");
+   }
+   //Test grilleValeurs
+   for(int i = 0; i < 12; i++) {
+      for(int j = 0; j < 12; j++) {
+         printf("%d", grilleValeurs[i][j]);
       }
       printf("\n");
    }
